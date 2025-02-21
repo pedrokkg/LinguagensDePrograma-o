1f3ec3d085c5018ef8c62e15f9a7047ba6444bcd
pedrokkg <pedrogarrao2004@gmail.com>
2025-02-21 19:29:49 -0300

    Delete 3.Média.py

--- 3.Média.py
+++
@@ -1,7 +0,0 @@
-#3. Média de Três Números
-n1=float(input("Insira o número 1: "))
-n2=float(input("Insira o número 2: "))
-n3=float(input("Insira o número 3: "))
-
-media=(n1+n2+n3)/3
-print(f"A média dos valores é de {media:.2f}")
