5487e70b379604d04be3c05ccf489139357d5acc
pedrokkg <pedrogarrao2004@gmail.com>
2025-02-21 19:25:20 -0300

    Add files via upload

---
+++ 4.Salario Mensal.py
@@ -0,0 +1,5 @@
+salario=float(input("Insira o valor seu salário: "))
+horas=int(input("Quantas horas você trabalha ao mês? "))
+
+result=salario/horas
+print(f"Você recebe R${result:.2f} por hora!")
