c1fa87483398f5148b874187b8cb3b6e152ad018
pedrokkg <pedrogarrao2004@gmail.com>
2025-02-21 19:29:55 -0300

    Delete 2.Temperatura.py

--- 2.Temperatura.py
+++
@@ -1,5 +0,0 @@
-#2. Conversão de Temperatura
-temperaturaCelsius=float(input("Insira a temperatura: "))
-temperaturaConvertida=(((temperaturaCelsius*9)/5)+32)
-
-print(f"{temperaturaCelsius:.2f}°C = {temperaturaConvertida:.2f}°C")
